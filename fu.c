3c50795c347ad5bdca2e2bd489fdf25b8b8cd176
fu-qingyun <1366773912@qq.com>
2022-04-08 19:29:36 +0800

    添加项目文件。

---
+++ fu.c
@@ -0,0 +1,69 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+//void tian_jia(char* p1, char* p2)
+//{
+//	char* pa1 = p1;
+//	char* pa2 = p2;
+//	while (*pa1!='\0')
+//	{
+//		pa1++;
+//	}
+//	while (*pa2!='\0')
+//	{
+//		*pa1 = *pa2;
+//		pa2++;
+//		pa1++;
+//	}
+//	*pa1 = '\0';
+//}
+//
+//int main()
+//{
+//	char arr1[10]="abcde";
+//	char arr2[5] = "1234";
+//	tian_jia(arr1, arr2);
+//	printf("%s", arr1);
+//	return 0;
+//}
+
+//#include <stdio.h>
+//int main()
+//{
+//	int n = 0;
+//	scanf("%d", &n);
+//	if (n <= 100 && n >= 0)
+//	{
+//		if (n >= 90)
+//		{
+//			printf("优\n");
+//		}
+//		else if (n >= 80)
+//		{
+//			printf("良\n");
+//		}
+//		else if (n >= 70)
+//		{
+//			printf("中\n");
+//		}
+//		else if (n >= 60)
+//		{
+//			printf("及\n");
+//		}
+//		else
+//		{
+//			printf("不及\n");
+//		}
+//	}
+//	else
+//	{
+//		printf("输入分数不合法\n");
+//	}
+//	return 0;
+//}
+
+#include <stdio.h>
+int main()
+{
+	return 0;
+}
